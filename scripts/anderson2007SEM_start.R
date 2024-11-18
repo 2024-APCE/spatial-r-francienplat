91fa2640c6e5ae48d9303fdebe52d88293daab30
Francien Plat <f.w.plat@student.rug.nl>
2024-11-18 11:43:08 +0100

    made model for P content

--- scripts/anderson2007SEM_start.R
+++ scripts/anderson2007SEM_start.R
@@ -10,6 +10,8 @@ rm(list = ls()) # clear environment
 
 library(tidyverse)
 # load the lavaan library
+install.packages("lavaan")
+#renv::snapshot() in console
 library(lavaan)
 
 # key variables of interest: 
@@ -28,12 +30,12 @@ library(lavaan)
 # LF_NA - plant leaf sodium content
 
 # dataset:
-# browseURL("https://docs.google.com/spreadsheets/d/1wk3UTAN7Cp7ZeoB0wpfW2C2eE_VoyKnJQpJ0Zrjk3yM/edit?usp=sharing")
+ #browseURL("https://docs.google.com/spreadsheets/d/1wk3UTAN7Cp7ZeoB0wpfW2C2eE_VoyKnJQpJ0Zrjk3yM/edit?usp=sharing")
 # read the data from the google docs link:
 Anderson2007<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQJ21uqYHZE0lUgVEou0Yp0HK_Hmjtokrmga8yx5mkV6rwWTNJUaFT9RABZtZqWUIhEArHGL7eIJY4O/pub?gid=1916700193&single=true&output=csv") %>%
   mutate(SOIL_RN=SOIL_NO3+SOIL_NH4)  # total soil reactive nitrogen
 names(Anderson2007)
-# standardize all variables to mean 0 and standard deviation 1
+# standardize all variables to mean 0 and standard deviation 1. to reflect the relative importance of the variables, instead of the individual slopes.
 Anderson2007std <- Anderson2007 |>
   mutate_all(~(scale(.) %>% as.vector)) |>
   as_tibble()
@@ -41,32 +43,47 @@ Anderson2007std
 # note that this does not affect the relations between the variables, only the scales  
 
 # make a pairs panel to inspect linearity of relations and expected normality of residuals
-psych::pairs.panels(Anderson2007 %>% select(ALL_LHU,RES_LHU,FIRE_FRQ,NMS,
-                                            LF_Na,LF_N),
+psych::pairs.panels(Anderson2007 %>% select(RES_LHU, BIOMASS,FIRE_FRQ,NMS,
+                                           LF_N),
                     stars = T, ellipses = F)
-psych::pairs.panels(Anderson2007std %>% select(BIOMASS,RES_LHU,FIRE_FRQ,NMS,
-                                            LF_Na,LF_N),
+psych::pairs.panels(Anderson2007std %>% select(RES_LHU, BIOMASS,FIRE_FRQ,NMS,
+                                                LF_N),
                     stars = T, ellipses = F)
 
 # analyse the model (response ~ predictors) with a multiple regression approach 
+multreg_std <- lm(LF_N ~ RES_LHU + BIOMASS + FIRE_FRQ + NMS, data = Anderson2007std)
+summary(multreg_std)
+
 
 # visualization of the result: 
 # browseURL("https://docs.google.com/presentation/d/1Q7uXC5Wiu0G4Xsp5uszCNHKOnf1IMI9doY-13Wbay4A/edit?usp=sharing")
 
 # Make a lavaan model as hypothesized in the Anderson et al 2007 paper and fit the model 
-
-
+Leaf_N_model<-'LF_N~BIOMASS+RES_LHU+FIRE_FRQ+NMS
+              BIOMASS~FIRE_FRQ+RES_LHU
+              NMS~FIRE_FRQ+RES_LHU'
+Leaf_N_model
+Leaf_N_fit<-lavaan::sem(Leaf_N_model, data=Anderson2007std)
 # show the model results
+summary(Leaf_N_fit,standardized=TRUE,fit.measures=T,rsquare=T)
 # goodness of fit (should be >0.9): CFI and TLI
 # badness of fit: ( should be <0.1): RMSEA, SRMR
+# if both are true, the model is good
 
-<<<<<<< HEAD
 # visualise the model
-=======
->>>>>>> 8a237fe2317acaad42b557f15ab08d729405ba65
+
+#same model but for phosphorus
+Leaf_P_model<-'LF_P~BIOMASS+RES_LHU+FIRE_FRQ+NMS
+              BIOMASS~FIRE_FRQ+RES_LHU
+              NMS~FIRE_FRQ+RES_LHU'
+Leaf_P_model
+Leaf_P_fit<-lavaan::sem(Leaf_P_model, data=Anderson2007std)
+# show the model results
+summary(Leaf_P_fit,standardized=TRUE,fit.measures=T,rsquare=T)
+
 
 # also explore the models as shown in fig 5b and 5c of the Anderson2007 paper
-# so repeat the model for leaf P content
+
 
 
 
